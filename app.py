223c3a5284def40b8c378bb7f9ca3cf18865f764
Michael Rutherford <itsmichaelharmon@gmail.com>
2025-01-12 21:35:39 -0600

    Renamed file and fixed formatting

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ class ImageProcessorApp:
     """
     Main application class that initializes and runs the image processor application.
     """
+
     def __init__(self):
         """
         Initializes the ImageProcessorApp with an image processor and UI.
@@ -29,6 +30,6 @@ if __name__ == "__main__":
     Entry point of the application that initializes the app and runs it.
     """
     app = QApplication(sys.argv)
-    window = ImageProcessorApp() 
+    window = ImageProcessorApp()
     window.run()
     sys.exit(app.exec())
